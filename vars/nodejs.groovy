f40a3835f3ae8c7ffd5d82c73e0d9a2187593978
Rakesh <dasarirakeshnaidu@gmail.com>
2023-05-13 15:59:16 +0530

    Updated the jenkins shared file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,21 +1,26 @@
-@Library('rakesh-shared-library') _
+def linkChecks(COMPONENT) {
+    sh '''
+                   echo installing jslint
+                   # npm install jslint
+                   # ls -ltr node_modules/jslint/bin/
+                   # node_modules/jslint/bin/jslint.js server.js
+                   echo performing lint checks
+                   echo performing lint checks completed"
+       '''           
+}
 
-pipeline {
-      agent any 
-      stages { 
-
-        stage( 'Performing lint checks') {
-            steps {
-                script {
-                    sample.info('SharedLibrary', 'stage.google.com')
+// Call is the default function which will be called when you call the filename
+def call(COMPONENT) {
+    pipeline {
+        agents any
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        nodejs.lintChecks(COMPONENT)
+                    }
                 }
-           }
-       }
-
-        stage ('Downloading the dependencies') {
-            steps {
-                sh "npm install"
-           }
+            }
         }
     }
 }
